15eb867fc6bac86c2bc0d0f65540d092f83b48d6
D <htehran333@gmail.com>
2024-04-02 10:35:49 -0600

    Outline Buttons are done

--- src/comps/Button.js
+++ src/comps/Button.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
-const Button = ({ to, label, light = false, customClass }) => {
-    const typeClasses = light ? "text-slate-600 button bg-slate-100 bg-gradient-to-br hover:bg-slate-200" : "text-slate-200 button bg-gray-700 bg-gradient-to-br hover:bg-slate-600";
-    const buttonClass = `transition duration-200 lg:text-sm text-center bg-opacity-80 hover:bg-opacity-90 text-[10px] rounded md:w-auto inline-block w-full px-10 py-3 ${typeClasses}`;
+const Button = ({ to, label, light = false, outline = false, customClass }) => {
+    const typeClasses = outline ? "text-slate-100 bg-transparent border-4 border-slate-100 hover:bg-slate-100 hover:text-slate-600" : light ? "text-slate-600 bg-slate-100" : "text-slate-200 bg-slate-700";
+    const buttonClass = `transition duration-200 lg:text-sm text-center backdrop-blur-sm bg-opacity-70 hover:bg-opacity-80 text-[12px] rounded md:w-auto inline-block w-full px-10 py-3 ${typeClasses}`;
 
     return (
         <Link to={to} className={`${buttonClass} ${customClass}`}>
