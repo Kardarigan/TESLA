ded38f887154d57604aaf750c1ebfe5c541ee99e
D <htehran333@gmail.com>
2024-04-09 12:09:09 -0700

    repair ow hero

--- src/comps/Ow/Ow_Hero.js
+++ src/comps/Ow/Ow_Hero.js
@@ -1,4 +1,5 @@
 import Background from "../../assets/Overviewpage-Group-83.jpg";
+import Button from "../Button";
 
 const Ow_Hero = () => {
   return (
@@ -6,9 +7,20 @@ const Ow_Hero = () => {
       className="fullSection"
       style={{ backgroundImage: "url(" + Background + ")" }}
     >
-      <div className="text-center grid gap-y-40">
-        <h1>Find Your Best, Then Customize It</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit</p>
+      <div className="text-center h-full">
+        <div className="text-center flex flex-col justify-between">
+          <div>
+            <h1>Find Your Best, Then Customize It</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit</p>
+          </div>
+          <div className="flex flex-col">
+            <Button to="/inventory" label="View Invertory" />
+
+            <p className="text-slate-300">
+              You Can Also Check Out the Inventory To Find Already Made Vehicles
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
