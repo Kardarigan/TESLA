2f9a3e6c57d703d5b14429afdeaf20a5fc95f08d
D <htehran333@gmail.com>
2024-04-13 07:33:16 -0700

    building safety section P2

--- src/comps/Car/Learn/Learn_Safety.js
+++ src/comps/Car/Learn/Learn_Safety.js
@@ -9,17 +9,20 @@ const Learn_Safety = (props) => {
         {carSafe.marks.map((item) => {
           return (
             <div
-              className={`safemark text-slate-50`}
-              style={{
-                "::before": {
-                  top: `${item.y}%`,
-                  left: `${item.x}%`,
-                },
-                "::after": {
-                  top: `${item.y}%`,
-                  left: `${item.x - 1}%`,
-                },
-              }}
+              className={`absolute size-full inset-0 text-slate-50
+                after:absolute
+                before:absolute
+                after:content-[""]
+                before:content-[""]
+                after:top-[15px]
+              after:bg-slate-50 after:block
+              after:w-[2px] after:h-[120px]
+              
+              before:top-[15px] before:block
+              before:w-[4px] before:h-[4px]
+              before:rounded-full before:ring-1
+            before:ring-slate-50 before:bg-slate-950
+              `}
             >
               {item.title}
             </div>
