c2fd3cf127ffce6de0fe1e7ef34b859841fab0b0
D <htehran333@gmail.com>
2024-04-14 05:28:40 -0700

    Ow Hero is Updated

--- src/comps/Ow/Ow_Hero.js
+++ src/comps/Ow/Ow_Hero.js
@@ -13,10 +13,12 @@ const Ow_Hero = () => {
             <h1>Find Your Best, Then Customize It</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit</p>
           </div>
-          <section className="pt-32">
-            <Button to="/inventory" label="View Invertory" />
-            <p>
-              You Can Also Check Out the Inventory To Find Already Made Vehicles
+          <section className="xl:pt-60 lg:pt-44 md:pt-24 sm:pt-64 pt-72">
+            <Button to="/inventory" label="View Invertory" light />
+            <p className="max-w-[600px] mx-auto sm:text-sm text-xs pt-3">
+              Lorem ipsum dolor sit amet consectetur, it amet consectetur,
+              adipisicing elit ipsum id! Tenetur inventore earum maiores neque
+              animi <u>et odit eligendi, sequi velit.</u>
             </p>
           </section>
         </div>
