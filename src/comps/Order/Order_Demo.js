cab22cca786629bb894d38e4cfed806b1c15fe01
D <htehran333@gmail.com>
2024-06-27 01:17:30 -0700

    Order Demo Done

--- src/comps/Order/Order_Demo.js
+++ src/comps/Order/Order_Demo.js
@@ -2,21 +2,25 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 const Order_Demo = ({ car }) => {
   return (
-    <section className="bg-slate-50 fixed w-2/3 h-full top-20">
+    <section className="bg-slate-50 fixed w-2/3 top-20 orderSlider overflow-hidden rounded-tr-xl">
       <Splide
         aria-label="Power Train"
         options={{
           type: "fade",
           rewind: true,
           autoplay: true,
-          arrows: false,
+          arrows: true,
           pagination: false,
         }}
       >
         {car.order.demo.map((item, index) => {
           return (
             <SplideSlide>
-              <img src={item} alt={index} className="bg-fullobject size-full" />
+              <img
+                src={item}
+                alt={index}
+                className="bg-fullobject scale-125 h-[88vh]"
+              />
             </SplideSlide>
           );
         })}
